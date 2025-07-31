2582b15e40b21f6fda7eade30514697010061f5f
Martin Rosenberg <2382147+MartinRosenberg@users.noreply.github.com>
2025-07-31 12:04:38 -0600

    Initial commit

---
+++ astro.config.ts
@@ -0,0 +1,18 @@
+import mdx from "@astrojs/mdx"
+import netlify from "@astrojs/netlify"
+import react from "@astrojs/react"
+import tailwindcss from "@tailwindcss/vite"
+import { defineConfig } from "astro/config"
+
+/**
+ * @see {@link https://astro.build/config}
+ */
+export default defineConfig({
+  adapter: netlify(),
+  integrations: [react(), mdx()],
+  site: "https://example.com", // TODO
+	trailingSlash: "always",
+  vite: {
+    plugins: [tailwindcss()]
+  },
+})
---
+++ prettier.config.mjs
@@ -0,0 +1,25 @@
+/**
+ * @import { Config } from "prettier"
+ */
+
+/**
+ * @see {@link https://prettier.io/docs/configuration}
+ * @see {@link https://prettier.io/docs/options}
+ */
+const config = /** @type {const} @satisfies {Config} */ ({
+	proseWrap: "always",
+	quoteProps: "consistent",
+	semi: false,
+	useTabs: true,
+	
+	overrides: [
+		{
+			files: ".github/*.md",
+			options: {
+				proseWrap: "preserve"
+			}
+		}
+	]
+})
+
+export default config
